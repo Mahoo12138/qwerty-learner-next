2aee6fbb57dc694a7389e48082a0edd70f16d28e
Mahoo12138 <mahoo12138@qq.com>
2025-02-07 17:24:01 +0800

    feat(web): add dark mode support

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,29 +1,38 @@
 // import { QueryClient } from '@tanstack/react-query'
-import Loading from '@/components/Loading'
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router'
+import Loading from "@/components/Loading";
+import { isOpenDarkModeAtom } from "@/store";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useAtomValue } from "jotai";
+import { useEffect } from "react";
 // import GeneralError from '@/features/errors/general-error'
 // import NotFoundError from '@/features/errors/not-found-error'
 
 export const Route = createRootRouteWithContext<{
   //   queryClient: QueryClient
-  token: String | null
+  token: String | null;
 }>()({
   component: () => {
+    const darkMode = useAtomValue(isOpenDarkModeAtom);
+    useEffect(() => {
+      darkMode
+        ? document.documentElement.classList.add("dark")
+        : document.documentElement.classList.remove("dark");
+    }, [darkMode]);
     return (
       <>
         <Outlet />
-        {import.meta.env.MODE === 'development' && (
+        {import.meta.env.MODE === "development" && (
           <>
             {/* <ReactQueryDevtools buttonPosition='bottom-left' /> */}
-            <TanStackRouterDevtools position='bottom-right' />
+            <TanStackRouterDevtools position="bottom-right" />
           </>
         )}
       </>
-    )
+    );
   },
   pendingComponent: Loading,
   //   notFoundComponent: NotFoundError,
   //   errorComponent: GeneralError,
-})
+});
