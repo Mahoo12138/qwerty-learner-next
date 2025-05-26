6e799a9b40ca1ab1dd6b8489d8e5694295cd5be9
Mahoo12138 <Mahoo12138@qq.com>
2025-05-26 19:47:09 +0800

    feat(web): 优化首页布局和功能
    - 调整 Header 组件样式，添加用户头像和等级
    - 重构 Home 组件，增加问候语和练习按钮
    - 更新首页样式，调整卡片布局
    - 新增颜色变量 --color-app-text

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "@tanstack/react-router";
 
 const Header: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <header className="z-20 mx-auto w-full px-10 py-6">
+    <header className="z-20 mx-auto w-full px-10 py-6 flex items-center justify-between">
       <div className="flex w-full flex-col items-center justify-between space-y-3 lg:flex-row lg:space-y-0">
         <Link
           className="flex items-center text-2xl font-bold text-indigo-500 no-underline hover:no-underline lg:text-4xl"
@@ -23,6 +23,16 @@ const Header: React.FC<PropsWithChildren> = ({ children }) => {
             {children}
           </nav>
         )}
+      </div>
+      <div className="relative element-shadow">
+        <img
+          alt="User Profile Picture"
+          className="w-12 h-12 sm:w-16 sm:h-16 rounded-full object-cover border-4 border-white"
+          src="https://avatars.githubusercontent.com/u/45908451"
+        />
+        <span className="absolute -bottom-1 -right-1 bg-app-primary text-white text-xs sm:text-xs font-bold px-1.5 py-0.5 sm:px-2 sm:py-1 rounded-full element-shadow">
+          Lvl 3
+        </span>
       </div>
     </header>
   );
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,49 +3,65 @@ import type React from "react";
 import { Keyboard, Bookmark, PlaneLanding } from "lucide-react";
 
 const Home: React.FC = () => {
+  const getGreeting = () => {
+    const hour = new Date().getHours();
+    if (hour < 12) {
+      return "Good morning";
+    }
+    if (hour < 18) {
+      return "Good afternoon";
+    }
+    return "Good evening";
+  };
+
+  const userName = "Mahoo"; // 后续根据接口展示
+
   return (
-    <div className="bg-app-background flex flex-col items-center justify-center">
-      <div className="container min-h-screen flex flex-col justify-between p-6 w-full">
-        <Header />
-        <main className="flex-grow flex flex-col items-center justify-center px-[100px]">
-          <div className="flex-grow flex flex-col items-center justify-center">
-            <button className="w-full max-w-sm bg-app-primary hover:bg-app-secondary transition-all text-white text-xl font-semibold py-5 px-6 rounded-xl element-shadow hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-app-secondary focus:ring-opacity-75 flex items-center justify-center space-x-3">
-              <Keyboard />
-              <span>Start Practice</span>
-            </button>
+    <div className="bg-app-background min-h-screen flex flex-col justify-between p-6">
+      <Header />
+      <main className="flex-grow flex flex-col items-center justify-center">
+        <div className="flex-grow flex flex-col items-center justify-center">
+          <div className="text-center mb-14">
+            <p className="text-5xl font-semibold text-app-text text-shadow">
+              {getGreeting()}, {userName}!
+            </p>
           </div>
-          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-14 mt-12 p-12 text-white">
-            <div className="bg-app-secondary backdrop-blur-sm p-6 rounded-xl element-shadow hover:border hover:border-app-primary transition-all cursor-pointer group">
-              <div className="flex items-center justify-between mb-3">
-                <h2 className="text-xl font-semibold text-white text-shadow group-hover:text-green-100 transition-colors">
-                  Dictionary
-                </h2>
-                <Bookmark />
-              </div>
-              <p className="text-white/80 text-sm text-shadow">
-                View your dictionaries
-              </p>
-              <div className="mt-4 h-2 bg-white/40 rounded-full overflow-hidden">
-                <div className="h-full bg-app-primary w-3/5"></div>
-              </div>
+          <button className="w-full max-w-sm bg-app-primary hover:bg-app-secondary transition-all text-white text-xl font-semibold py-5 px-6 rounded-xl element-shadow hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-app-secondary focus:ring-opacity-75 flex items-center justify-center space-x-3">
+            <Keyboard />
+            <span>Start Practice</span>
+          </button>
+        </div>
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-14 mt-12 p-12 text-white">
+          <div className="bg-app-secondary backdrop-blur-sm p-6 rounded-xl element-shadow hover:border hover:border-app-primary transition-all cursor-pointer group">
+            <div className="flex items-center justify-between mb-3">
+              <h2 className="text-xl font-semibold text-white text-shadow group-hover:text-green-100 transition-colors">
+                Dictionary
+              </h2>
+              <Bookmark />
             </div>
-            <div className="bg-app-secondary backdrop-blur-sm p-6 rounded-xl element-shadow hover:border hover:border-app-primary transition-all cursor-pointer group">
-              <div className="flex items-center justify-between mb-3">
-                <h2 className="text-xl font-semibold text-white text-shadow group-hover:text-green-100 transition-colors">
-                  Dashboard
-                </h2>
-                <PlaneLanding />
-              </div>
-              <p className="text-white/80 text-sm text-shadow">
-                View your progress
-              </p>
-              <div className="mt-4 h-2 bg-white/40 rounded-full overflow-hidden">
-                <div className="h-full bg-app-primary w-4/5"></div>
-              </div>
+            <p className="text-white/80 text-sm text-shadow">
+              View your dictionaries
+            </p>
+            <div className="mt-4 h-2 bg-white/40 rounded-full overflow-hidden">
+              <div className="h-full bg-app-primary w-3/5"></div>
             </div>
           </div>
-        </main>
-      </div>
+          <div className="bg-app-secondary backdrop-blur-sm p-6 rounded-xl element-shadow hover:border hover:border-app-primary transition-all cursor-pointer group">
+            <div className="flex items-center justify-between mb-3">
+              <h2 className="text-xl font-semibold text-white text-shadow group-hover:text-green-100 transition-colors">
+                Dashboard
+              </h2>
+              <PlaneLanding />
+            </div>
+            <p className="text-white/80 text-sm text-shadow">
+              View your progress
+            </p>
+            <div className="mt-4 h-2 bg-white/40 rounded-full overflow-hidden">
+              <div className="h-full bg-app-primary w-4/5"></div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
